b5a0303445414ed8a8cde573f1b791ba1c877736
wsxq123 <wsxq123@gmail.com>
2024-08-09 08:04:31 +0800

    test

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <Button>test</Button>
+        <p>77777</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
